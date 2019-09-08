f439a02ed6a2fcd88da63f02679151f9d358b31b
Ganesh <jogiganesh@gmail.com>
2019-09-08 21:41:59 +0530

    ok

---
+++ features/login.feature
@@ -0,0 +1,5 @@
+Feature: validate login paaage
+Scenario: verify login module
+Given valid url
+When valid id & pass
+Then click on liogin
---
+++ src/main/java/profile/Login.java
@@ -0,0 +1,35 @@
+package profile;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Login {
+	
+public static WebDriver driver;
+	
+	@Given("^valid url$")
+	public void url() throws Exception{
+		System.setProperty("webdriver.chrome.driver","F:\\Prasanna\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		
+		
+	}
+	@When("^valid id & pass$")
+	public void credintials() throws Exception {
+		driver.findElement(By.name("email")).sendKeys("kumarklr5@gmailcom");
+		driver.findElement(By.name("pass")).sendKeys("KUMAR@0302");
+		
+	}
+	@Then("^click on liogin$")
+	public void login() throws Exception {
+		driver.findElement(By.id("u_0_2")).click();
+		
+	}
+
+}
